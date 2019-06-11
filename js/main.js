80f6d46cad398c10b44c9c2b78a27ea5fe335dff
edjunma <junwenma@buffalo.edu>
2019-06-11 11:42:24 -0400

    adding main time functions

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// DOM Elements
+const time = document.getElementById('time'),
+	greeting = document.getElementById('greeting'),
+	name = document.getElementById('name'),
+	focus = document.getElementById('focus');
+
+// Show Time
+function showTime() {
+	let today = new Date(),
+		hour = today.getHours(),
+		min = today.getMinutes(),
+		sec = today.getSeconds();
+
+	// Set AM or PM
+	const amPM = hour >= 12 ? 'PM' : 'AM';
+
+	// 12-hour Format
+	hour = hour % 12 || 12;
+
+	// Output Time
+	time.innerHTML = `${hour}<span>:</span>?{min}<span>:</span>${sec}`;
+
+	setTimeout(showTime, 1000);
+}
+
+// Run
+showTime();
